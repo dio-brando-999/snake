aead4ae12611d84b613fe39d19978804b79d2f84
dio_brando_999 <brandon.zau2006@gmail.com>
2025-02-17 23:09:52 -0500

    updated move, change_direction, and check_out_of_bounds functions for the snake logic

--- snake.py
+++ snake.py
@@ -1,14 +1,21 @@
+#This Class snake represents the snake. It will be responsible for the following:
+#tracking the snake's position (stored as a list of tuples, which represent the coordinate of each body segement)
+#handling movement (adding a new head and removing the tail given a boolean value representing if its eaten or not)
+#changing direction (but preventing it from turning backward)
+#detecting self-collisions (when it runs into itself) and detecting when it is out of bounds
+
 class Snake:
     #__init__: Tuple Tuple -> None
     def __init__(self, start_pos, start_direction):
         self.body = [start_pos]
         self.direction = start_direction
-    #move: None -> None
+    #move: Boolean -> None
     #updates a snakes 
-    def move(self) -> None:
+    def move(self, ate_food) -> None:
         newHead=(self.body[0][0]+self.direction[0],self.body[0][1]+self.direction[1])
         self.body.insert(0, newHead)
-        self.body.pop()
+        if not ate_food:
+            self.body.pop()
     
     #check_for_collison: None -> Boolean
     #checks the snake if it has collided with itself or not
@@ -20,18 +27,18 @@ class Snake:
             return True
         else:
             return False
-
+    #change_direction: Tuple[Int][Int] -> None
+    #changes the direction given a tuple direction
+    def change_direction(self, new_direction):
+        if new_direction[0]==self.direction[0] and new_direction[0] == -self.direction[1]:
+            return 
+        else:
+            self.direction = new_direction
     
-snake1 = Snake((10,10), (0, -1))
-snake2 = Snake((5,5), (0, -1))
-
-snake1.body.append((11,10))
-print("Snake 1 Body,:", snake1.body)
-print("Snake 2 Body,:", snake2.body)
-#This Class snake represents the snake. It will be responsible for the following:
-#Tracking the snake's position (stored as a list of tuples).
-#Handling movement (adding a new head and removing the tail).
-#Changing direction (but preventing it from turning backward).
-#Detecting self-collisions (when it runs into itself).
-#Growing when it eats food.
-    #def move(self):
+    #check_out_of_bounds: Tuple[Int][Int] -> Boolean
+    def check_out_of_bounds(self,world_size):
+        head_x = self.body[0][0]
+        head_y = self.body[0][1]
+        width = world_size[0]
+        height = world_size[1]
+        return not (0 <= head_x < width and 0 <= head_y < height)
